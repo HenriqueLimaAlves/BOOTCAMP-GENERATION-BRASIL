eadaabc894f833f10288f575bf885299a6f5afae
Hnerique <henri633@gmail.com>
2020-08-07 17:05:20 -0300

    03_Calcule a Expressao

---
+++ EXERCICIOS/01_introducao_java/src/calculeAExpressao.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.lang.Math;
+import java.util.Locale;
+public class calculeAExpressao {
+	public static void main(String args[]) {
+		
+		Scanner ler = new Scanner(System.in);
+		
+		double A, B, C, R, S, D;
+		
+		System.out.println("Digite o valor de A: ");
+		A = ler.nextInt();
+		
+		System.out.println("Digite o valor de B: ");
+		B = ler.nextInt();
+		
+		System.out.println("Digite o valor de B: ");
+		C = ler.nextInt();
+		
+		R = Math.pow((A+B), 2.0);
+		S = Math.pow((C+B), 2.0);
+		D = R+S/2;
+		
+		Locale.setDefault(Locale.US);
+		
+		System.out.printf("O resultado de D=R+S/2 é: %.2f", D);
+	}
+}
